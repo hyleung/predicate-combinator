7b251e355f2e36f5dc338f2d7f4fc1488632ea98
Ho Yan Leung <hy.leung@gmail.com>
2015-06-02 20:36:40 -0700

    Refactors test

--- src/test/scala/PredicateSpec.scala
+++ src/test/scala/PredicateSpec.scala
@@ -4,27 +4,28 @@ import org.scalatest._
 class PredicateSpec extends FlatSpec with Matchers {
 	behavior of "Predicate operator"
 	"'or' operator" should "filter list for elements that satisfy any predicate" in {
-		val even = Predicate[Int]( i => i % 2 == 0 )
-		val divisibleBy5 = Predicate[Int]( i => i % 5 == 0)
+		val even = divisibleBy(2)
+		val divisibleBy5 = divisibleBy(5)
 
 		List(3, 5, 2).filter(even or divisibleBy5 ) should be (List(5, 2))
 		List(3, 5, 7).filter(even or divisibleBy5 ) should be (List(5))
 		List(3, 1, 7).filter(even or divisibleBy5 ) should be (List())
 	}
 	"'and' operator" should "filter list for elements that satisfy all predicates" in {
-		val even = Predicate[Int]( i => i % 2 == 0 )
-		val divisibleBy5 = Predicate[Int]( i => i % 5 == 0)
+		val even = divisibleBy(2)
+		val divisibleBy5 = divisibleBy(5)
 
 		List(3, 5, 2).filter(even and divisibleBy5 ) should be (List())
 		List(3, 5, 7, 10).filter(even and divisibleBy5 ) should be (List(10))
 	}
 	"! operator" should "negate a predicate" in {
-		val even = Predicate[Int]( i => i % 2 == 0 )
+		val even = divisibleBy(2)
 		val odd = !even
-		val divisibleBy5 = Predicate[Int]( i => i % 5 == 0)
+		val divisibleBy5 = divisibleBy(5)
 
 		List(3, 5, 2).filter(odd and divisibleBy5 ) should be (List(5))
 	}
 
+	def divisibleBy(i:Int):Predicate[Int] = Predicate( x => x % i == 0)
 
 }
